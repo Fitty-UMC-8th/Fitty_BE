04320a692ee0daf183485ff3d314c62352eb2baa
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-09-04 19:29:08 +0900

    feat: 초기 세팅

---
+++ src/main/java/umc/fitty/FittyApplication.java
@@ -0,0 +1,13 @@
+package umc.fitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FittyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc/fitty/FittyApplicationTests.java
@@ -0,0 +1,13 @@
+package umc.fitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
