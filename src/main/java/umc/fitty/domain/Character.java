da3a52d9a7c847e8515f5fdf4c973cd03ac141d8
hoseong <hoseong691@gmail.com>
2025-09-23 19:39:14 +0900

    feat: 캐릭터 생성 기능 구현 중

--- src/main/java/umc/fitty/domain/Character.java
+++ src/main/java/umc/fitty/domain/Character.java
@@ -1,4 +1,36 @@
 package umc.fitty.domain;
 
-public class Character {
+import jakarta.persistence.*;
+import lombok.*;
+import umc.fitty.domain.common.BaseEntity;
+import umc.fitty.domain.enums.CharacterType;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Character extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String name;
+
+    @Builder.Default
+    private int level = 1;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private CharacterType type;
+
+    private void setUser(User user) {
+        this.user = user;
+    }
+
 }
